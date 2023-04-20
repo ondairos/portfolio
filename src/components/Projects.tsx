ed22ee555ca607fb1c8357ba1b958ed71e89c228
Ondairos <ondairos@gmail.com>
2023-04-20 14:54:45 +0300

    css to projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,53 +4,47 @@ import "../App.css";
 const Projects = () => {
   return (
     <div id="sect2" className="box section2">
-      <h1>Projects:</h1>
-      <h4>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
-        nisi cupiditate delectus veniam sit similique perferendis. Nobis, at
-        aspernatur aperiam corrupti delectus, odit quibusdam voluptate sapiente
-        molestias sequi deserunt quos.
-      </h4>
+      <h1 className="section2_header">Projects:</h1>
       <div className="sect2_container">
         <div className="sect2_product">
           <img src="https://via.placeholder.com/250x250" alt="Project 1"></img>
-          <h3>Project 1</h3>
-          <p>$20.00</p>
+          <h3>Tenzies Dice Game</h3>
+          <p>Github Link</p>
         </div>
         <div className="sect2_product">
           <img src="https://via.placeholder.com/250x250" alt="Project 2"></img>
-          <h3>Project 2</h3>
-          <p>$25.00</p>
+          <h3>Hearthstone Festival of Legends</h3>
+          <p>Github Link</p>
         </div>
         <div className="sect2_product">
           <img src="https://via.placeholder.com/250x250" alt="Project 3"></img>
           <h3>Project 3</h3>
-          <p>$30.00</p>
+          <p>Github Link</p>
         </div>
         <div className="sect2_product">
           <img src="https://via.placeholder.com/250x250" alt="Project 4"></img>
           <h3>Project 4</h3>
-          <p>$35.00</p>
+          <p>Github Link</p>
         </div>
         <div className="sect2_product">
           <img src="https://via.placeholder.com/250x250" alt="Project 5"></img>
           <h3>Project 5</h3>
-          <p>$40.00</p>
+          <p>Github Link</p>
         </div>
         <div className="sect2_product">
           <img src="https://via.placeholder.com/250x250" alt="Project 6"></img>
           <h3>Project 6</h3>
-          <p>$45.00</p>
+          <p>Github Link</p>
         </div>
         <div className="sect2_product">
           <img src="https://via.placeholder.com/250x250" alt="Project 7"></img>
           <h3>Project 7</h3>
-          <p>$50.00</p>
+          <p>Github Link</p>
         </div>
         <div className="sect2_product">
           <img src="https://via.placeholder.com/250x250" alt="Project 8"></img>
           <h3>Project 8</h3>
-          <p>$55.00</p>
+          <p>Github Link</p>
         </div>
       </div>
     </div>
