21ea1251a5fa6ccdf94d5c105021cd188ce78f3f
Ondairos <ondairos@gmail.com>
2023-04-20 16:57:00 +0300

    added starter files

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import TechStack from "./components/TechStack";
 import Footer from "./components/Footer";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <About />
       <Projects />
       <TechStack />
+      <Contact />
       <Footer />
     </div>
   );
---
+++ src/components/Contact.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css";
+
+const Contact = () => {
+  return (
+    <div id="sect4" className="box section4">
+      <h1>Contact me: </h1>
+      <div>test</div>
+    </div>
+  );
+};
+
+export default Contact;
