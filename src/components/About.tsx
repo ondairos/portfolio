7806f7c73c4320d048920fe3b9bdae34973bd308
Ondairos <ondairos@gmail.com>
2023-04-20 16:48:05 +0300

    changes to picture

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,9 +6,7 @@ const About = () => {
   return (
     <div id="sect1" className="box section1">
       <div className="section1_textAbout">
-        <div className="section1_pic_div">
-          <img src={iconPhoto} alt="" className="section1_pic" />
-        </div>
+        <img src={iconPhoto} alt="" className="section1_pic" />
         <p className="section1_text">
           Hi, I am <span className="section_name_span">John.</span> <br />
           I'm a Junior Full Stack Developer <br /> passionate about creating
