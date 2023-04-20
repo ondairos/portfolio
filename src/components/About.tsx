bcdc32f8908f032211e67cbc030b4449bc661bc2
Ondairos <ondairos@gmail.com>
2023-04-20 17:42:19 +0300

    finished about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -15,7 +15,27 @@ const About = () => {
       </div>
       <div className="section1_socials">
         <p className="section1_socials_l">
-          LinkedIn | Twitter | Instagram | Github
+          <a
+            href="https://www.linkedin.com/in/ioannis-kantiloros-2438b6153/"
+            className="section1_linkedin"
+          >
+            LinkedIn
+          </a>{" "}
+          |{" "}
+          <a href="https://github.com/ondairos" className="section1_github">
+            GitHub
+          </a>{" "}
+          |{" "}
+          <a href="https://twitter.com/Ondairos" className="section1_twitter">
+            Twitter
+          </a>{" "}
+          |{" "}
+          <a
+            href="https://www.instagram.com/giannis_kanti/"
+            className="section1_instagram"
+          >
+            Instagram
+          </a>
         </p>
       </div>
     </div>
