17c17aeddac55e284b9ed8bd2bd0dd4016d5da6d
Ondairos <ondairos@gmail.com>
2023-04-20 14:43:22 +0300

    add

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,14 +6,17 @@ const About = () => {
   return (
     <div id="sect1" className="box section1">
       <div className="section1_textAbout">
-        <div className="section1_pic_div"></div>
-        <img src={iconPhoto} alt="" className="section1_pic" />
+        <div className="section1_pic_div">
+          <img src={iconPhoto} alt="" className="section1_pic" />
+        </div>
         <p className="section1_text">
           Hi, I am John. <br />
           I'm a Junior Full Stack Developer using the MERN Stack and building
           cool and elegant applications.
           <br />
-          I'm currently working at Lorem Ipsum.
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint eum
+          dolor quam, earum amet sit porro eius esse natus. Laborum non ab
+          excepturi sapiente fugit? Porro accusantium voluptates assumenda quia!
         </p>
       </div>
       <div className="section1_socials">
