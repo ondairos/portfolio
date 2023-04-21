1b362cfa1b720db01d038d84ca2f3ea5c7b572c2
Ondairos <ondairos@gmail.com>
2023-04-21 18:38:32 +0300

    css changes

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,8 +4,42 @@ import "../App.css";
 const Contact = () => {
   return (
     <div id="sect4" className="box section4">
-      <h1>Contact me: </h1>
-      <div>test</div>
+      <section className="contact">
+        <div className="contact-form">
+          <h1>
+            Contact <span>Me</span>
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
+            voluptatibus architecto, hic totam dignissimos veniam? Sit sapiente
+            quod aperiam quia atque maxime, placeat cumque eligendi porro, unde
+            veniam dignissimos dicta.
+          </p>
+          <form action="">
+            <input type="text" placeholder="Your Name" required />
+            <input
+              type="email"
+              name="email"
+              id=""
+              placeholder="E-mail"
+              required
+            />
+            <input type="" placeholder="Write a Subject" required />
+            <textarea
+              name=""
+              id=""
+              cols={parseInt("30")}
+              rows={parseInt("10")}
+              placeholder="Your Message"
+              required
+            ></textarea>
+            <input type="submit" name="" value="Submit" className="btn" />
+          </form>
+          <div className="contact-img">
+            <s></s>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,35 +1,69 @@
 import "../App.css";
 // import tech stack icons
 import ReactIcon from "../assets/tsicons/react-js-icon.svg";
+import HtmlIcon from "../assets/tsicons/html-icon.svg";
+import CSSIcon from "../assets/tsicons/css-icon.svg";
+import JSIcon from "../assets/tsicons/javascript-programming-language-icon.svg";
+import TSIcon from "../assets/tsicons/typescript-programming-language-icon.svg";
+
 import NodeIcon from "../assets/tsicons/node-js-icon.svg";
 import MongoIcon from "../assets/tsicons/mongodb-icon.svg";
 import ExpressIcon from "../assets/tsicons/express-js-icon.svg";
+import MySQLIcon from "../assets/tsicons/mysql-icon.svg";
+import PythonIcon from "../assets/tsicons/python-programming-language-icon.svg";
+
+import gitIcon from "../assets/tsicons/git-icon.svg";
+import dockerIcon from "../assets/tsicons/docker-icon.svg";
 
 const TechStack = () => {
   return (
     <div id="sect3" className="box section3">
-      <h1>Technologies:</h1>
+      <h1>Technologies</h1>
       <div className="section3_container">
         <div className="section3_col1">
           <div className="section3_frontend">
             <div className="section3_col1_header">
-              <h3>Frontend:</h3>
+              <h3>Frontend</h3>
             </div>
             <div className="section3_col1_frontend_icontainer">
               <div className="section3_col1_fr_ip">
                 <div className="image">
+                  <img src={HtmlIcon} alt="HtmlIcon" />
+                </div>
+                <p>HTML</p>
+              </div>
+              <div className="section3_col1_fr_ip">
+                <div className="image">
+                  <img src={CSSIcon} alt="CSSIcon" />
+                </div>
+                <p>CSS</p>
+              </div>
+              <div className="section3_col1_fr_ip">
+                <div className="image">
                   <img src={ReactIcon} alt="ReactIcon" />
                 </div>
-                <p>React JS</p>
+                <p>ReactJS</p>
+              </div>
+              <div className="section3_col1_fr_ip">
+                <div className="image">
+                  <img src={JSIcon} alt="JSIcon" />
+                </div>
+                <p>JavaScript</p>
+              </div>
+              <div className="section3_col1_fr_ip">
+                <div className="image">
+                  <img src={TSIcon} alt="TSIcon" />
+                </div>
+                <p>TypeScript</p>
               </div>
             </div>
           </div>
         </div>
-        {/* ================ */}
+        {/* =======BACKEND========= */}
         <div className="section3_col1">
           <div className="section3_frontend">
             <div className="section3_col1_header">
-              <h3>Backend:</h3>
+              <h3>Backend</h3>
             </div>
             <div className="section3_col1_frontend_icontainer">
               <div className="section3_col1_fr_ip">
@@ -40,31 +74,49 @@ const TechStack = () => {
               </div>
               <div className="section3_col1_fr_ip">
                 <div className="image">
+                  <img src={ExpressIcon} alt="ExpressIcon" />
+                </div>
+                <p>ExpressJS</p>
+              </div>
+              <div className="section3_col1_fr_ip">
+                <div className="image">
                   <img src={MongoIcon} alt="MongoIcon" />
                 </div>
                 <p>MongoDB</p>
               </div>
               <div className="section3_col1_fr_ip">
                 <div className="image">
-                  <img src={ExpressIcon} alt="ExpressIcon" />
+                  <img src={MySQLIcon} alt="MySQLIcon" />
                 </div>
-                <p>ExpressJS</p>
+                <p>MySQL</p>
+              </div>
+              <div className="section3_col1_fr_ip">
+                <div className="image">
+                  <img src={PythonIcon} alt="PythonIcon" />
+                </div>
+                <p>Python</p>
               </div>
             </div>
           </div>
         </div>
-        {/* ============ */}
+        {/* =======OTHER========= */}
         <div className="section3_col1">
-          <div className="section3_frontend">
+          <div className="section3_frontend s3other">
             <div className="section3_col1_header">
-              <h3>Other:</h3>
+              <h3>Other</h3>
             </div>
             <div className="section3_col1_frontend_icontainer">
               <div className="section3_col1_fr_ip">
                 <div className="image">
-                  <img src={ReactIcon} alt="ReactIcon" />
+                  <img src={gitIcon} alt="gitIcon" />
                 </div>
-                <p>React JS</p>
+                <p>Git</p>
+              </div>
+              <div className="section3_col1_fr_ip">
+                <div className="image">
+                  <img src={dockerIcon} alt="dockerIcon" />
+                </div>
+                <p>Docker</p>
               </div>
             </div>
           </div>
