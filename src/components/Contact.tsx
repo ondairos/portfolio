43c60cf494e3991d81868ad700f7993d34339ac2
Ondairos <ondairos@gmail.com>
2023-04-24 16:43:19 +0300

    add concat box

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "../App.css";
+import email_icon from "../assets/tsicons/email-envelop-open-icon.svg";
+import linkedin_icon from "../assets/tsicons/linkedin-square-icon.svg";
 
 const Contact = () => {
   return (
@@ -7,9 +9,34 @@ const Contact = () => {
       <section className="contact">
         <div className="contact-form">
           <h1>
-            Contact <span>me</span>
+            Contact <span>Me</span>
           </h1>
-          <h5>Email: jkantiloros [at] gmail.com</h5>
+          <div className="contact-form_info_main_container">
+            <div className="contant-form_info_container">
+              <img
+                src={email_icon}
+                alt="Email icon"
+                className="contact_email_icon"
+              />
+              <p>
+                <a href="mailto:jkantiloros@gmail.com">
+                  jkantiloros[at]gmail.com
+                </a>
+              </p>
+            </div>
+            <div className="contant-form_info_container">
+              <img
+                src={linkedin_icon}
+                alt="linkedin icon"
+                className="contact_linkedin_icon"
+              />
+              <p>
+                <a href="https://www.linkedin.com/in/ioannis-kantiloros-2438b6153/">
+                  LinkedIn
+                </a>
+              </p>
+            </div>
+          </div>
           <form action="">
             <input type="text" placeholder="Your Name" required />
             <input
