26f8542f1360bd5e2b81e91106c29dd757f83c19
Ondairos <ondairos@gmail.com>
2023-06-26 12:08:22 +0300

    added animejs

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "../App.css";
 import iconPhoto from "../assets/IconPhotoPX.jpg";
+// animejs
+// import anime from "animejs/lib/anime.es.js";
 
 const About = () => {
   return (
     <div id="sect1" className="box section1">
       <div className="section1_textAbout">
         <img src={iconPhoto} alt="" className="section1_pic" />
-
         <section className="section1_text">
           <h2 className="greeter">
             Hi, I am <span className="section_name_span">John.</span> <br />
